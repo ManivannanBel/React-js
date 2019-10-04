d4bbe721927c996235c3843fc8d7709b44c31a0b
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-10-04 21:47:35 +0530

    class component

---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,11 @@
+//Class Component
+
+import React, { Component } from 'react'
+
+class Welcome extends Component{
+    render(){
+        return <div><h1>Welcome</h1><p>Class Component</p></div>
+    }
+}
+
+export default Welcome
